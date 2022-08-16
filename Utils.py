53064852544d71c6e27b3ac317faf02a5d6d5d1e
Andrew Muhsen Paramedis <92538925+Andrew2077@users.noreply.github.com>
2022-08-16 23:02:31 +0200

    Initial commit

---
+++ Utils.py
@@ -0,0 +1,176 @@
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+
+
+age_dict = {
+    '100+': 20,
+    '95-99': 19,
+    '90-94': 18,
+    '85-89': 17,
+    '80-84': 16,
+    '75-79': 15,
+    '70-74': 14,
+    '65-69': 13,
+    '60-64': 12,
+    '55-59': 11,
+    '50-54': 10,
+    '45-49': 9,
+    '40-44': 8,
+    '35-39': 7,
+    '30-34': 6,
+    '25-29': 5,
+    '20-24': 4,
+    '15-19': 3,
+    '10-14': 2,
+    '5-9': 1,
+    '0-4': 0
+}
+
+countries_dict = {
+    'PT': 'Portugal',
+    'NL' : 'Netherlands',
+    'AU' : 'Australia',
+    'CA' : 'Canada',
+    'ES' : 'Spain',
+    'IT' : 'Italy',
+    'GB' : 'United Kingdom',
+    'FR' : 'France',
+    'DE' : 'Germany',
+    'US' : 'United States',
+}
+
+def bar_coloring(bar, ax):
+    bar_color = bar[0].get_facecolor()
+    height = (bar[0].get_height()/100)*2
+    for bar in bar:
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,
+            bar.get_height() + height,
+            int(round(bar.get_height(), 0)),
+            horizontalalignment='center',
+            color=bar_color,
+            fontsize=7,
+            weight='bold'
+        )
+
+def dejunking(ax,fig):
+        
+    x = plt.gca().xaxis
+    for item in x.get_ticklabels():
+        item.set_fontsize(7.5)
+        item.set_rotation(45)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_color('#DDDDDD')
+    ax.tick_params(bottom=False, left=False)
+
+    ax.set(facecolor='lightgray')
+    fig.set(facecolor='lightgray')
+
+    plt.grid(axis='x', visible=False)
+    ax.get_yaxis().set_ticks([])
+    fig.tight_layout()
+    ax.margins(x=0.01)
+    ax.margins(y=0.005)
+
+    plt.legend(loc ='best')
+    
+
+def bar_plot(df, choosen_gender, destination):
+    if choosen_gender == 'both':
+        cond2 = df['country_destination'] == destination
+        df1 = df[cond2]
+        width = 0.85
+
+    else:
+        cond1 = df['gender'] == choosen_gender
+        cond2 = df['country_destination'] == destination
+        df1 = df[cond1 & cond2]
+        width = 1.3
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    bar_width = 0
+    for element in df1.gender.unique():
+        if element == 'male':
+            color = '#1f77b4'
+        elif element == 'female':
+            color = '#d62728'
+        bars = plt.bar(
+            x=(df1[df1['gender'] == element]['values']+bar_width)*2,
+            height=df1[df1['gender'] == element]['population_in_thousands'],
+            label=element,
+            width=width,
+            color=color,
+        )
+        print(bar_width)
+        bar_width = 0.5
+        bar_coloring(bars, ax)
+
+    dejunking(ax, fig)
+    if choosen_gender == 'both':
+        plt.xticks(df1['values']*2+0.5, df1['age_bucket'])
+    else:
+        plt.xticks(df1['values']*2+0.5, df1['age_bucket'])
+    plt.legend(loc='best')
+
+    plt.title("Flights of {}s to {}".format(
+        choosen_gender, countries_dict[destination]), fontsize=23, pad=35)
+    plt.xlabel('Age Bucket', fontsize=17,)
+    plt.ylabel('Population in Thousands', fontsize=17,)
+    plt.show()
+    st.pyplot(fig)
+    
+
+def box_plot(df, choosen_gender, destination, orientation='v'):
+    if orientation == 'v':
+        x_axies = {'gender' : "Gender"}
+        y_axies = {'population_in_thousands' : "Population in Thousands"}
+    elif orientation == 'h':
+        y_axies = {'gender' : "Gender"}
+        x_axies = {'population_in_thousands' : "Population in Thousands"}
+
+    theme = ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]
+    if choosen_gender == 'both':
+        cond2 = df['country_destination'] == destination
+        df1 = df[cond2]
+
+    else:
+        cond1 = df['gender'] == choosen_gender
+        cond2 = df['country_destination'] == destination
+        df1 = df[cond1 & cond2]
+
+        
+    fig = px.box(df1, x =list(x_axies.keys())[0], y = list(y_axies.keys())[0], points= 'all', 
+                color = 'gender',template= theme[2], hover_data=['age_bucket'  , 'population_in_thousands'],
+                labels = {'population_in_thousands': 'Population ', 'age_bucket': 'Age ',
+                        'gender': 'Gender '},color_discrete_map={ "male": "#1f77b4", "female": "#d62728"
+                },
+                notched=False,)
+             
+
+    fig.update_layout(height=600, width=800)
+    fig.update_layout(title_text='Flights of {}s to {}'.format(choosen_gender, destination), title_x=0.5)
+    # fig.show()
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+def heatmap_plottly(df):
+    theme = ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]
+    fig = px.imshow(df.corr().round(3), text_auto="True", template=theme[2],
+                    color_continuous_scale="ylgnbu", aspect='auto', range_color=[-1, 1])
+
+
+    fig.update_layout(height=600, width=800)
+    fig.update_yaxes(tickangle=300, tickfont=dict(size=18))
+    fig.update_xaxes(tickfont=dict(size=18))
+    fig.update_layout(margin_pad=10)
+    fig.update_layout(font_size=18)
+    fig.update_layout(title_text='Correlation between columns', title_x=0.5)
+    st.plotly_chart(fig, use_container_width=True)
+    
+    
---
+++ ✈️_Airbnb.py
@@ -0,0 +1,348 @@
+from streamlit.logger import get_logger
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import streamlit as st
+import pandas as pd
+
+from Utils import *
+
+
+plt.rcdefaults()
+plt.style.use('seaborn-darkgrid')
+color_list = mpl.rcParams['axes.prop_cycle']
+color_list = list(color_list.by_key().values())[0]
+
+
+LOGGER = get_logger(__name__)
+
+# 🏠 📊 📈 📉 ⚠️🔒 📁  📂 💻 ✈️ 🏁
+
+
+def run():
+
+    st.set_page_config(
+        page_title="Air bnb Data Visualization",
+        page_icon="✈️",
+        layout="wide",
+    )
+    # st.write('## this is our Work Documentation ✈️', anchor='#header1')
+    # st.header("## Table of Contents")
+
+    st.write("# this is our Work Documentation ✈️")
+
+
+#*  ================================================== Side bar==============================================================
+    show_list = st.sidebar.checkbox('show list', False)
+    st.markdown("""## $\color{#1f77b4}{\t{AgeGender DataSet}}$
+```python
+age_gender = pd.read_csv('airbnb/age_gender_bkts.csv') #* reading 1st dataframe
+# * dropping year column - not needed
+age_gender.drop(['year'], axis=1, inplace=True)
+st.age_gender
+```
+                """)
+    
+#* =================================================== 1- Age gender DF ===========================================================
+    age_gender = pd.read_csv('airbnb/age_gender_bkts.csv')  # * reading 1st dataframe
+    # * dropping year column - not needed
+    age_gender.drop(['year'], axis=1, inplace=True)
+    st.dataframe(age_gender, width=1000)
+    st.write('\n')
+
+    st.markdown("""
+### After taking a look at the previous sample data,
+
+- The data is clean, no missing values
+- year column is redundant
+- visualizing the data is going to be mainly done on population_in_thousands
+
+```python
+age_gender['population_in_thousands'] #values = 420, unique = 381
+
+```
+""")
+    st.write('''now let's take a look at the unique values of dataset - apply show list to show output''')
+    st.markdown("""
+```python
+print("Age Bucket : ", age_gender.age_bucket.unique())
+print("Countries : ",age_gender.country_destination.unique())
+print('Gender :',age_gender.gender.unique())
+```
+
+""")
+#* =====================================================show list================================================================================================================
+    if show_list:
+        st.write("Age Bucket : ", list(age_gender.age_bucket.unique()))
+        st.write("Countries: ", list(age_gender.country_destination.unique()))
+        st.write('Gender :', list(age_gender.gender.unique()))
+
+    st.markdown("""
+# Plotting the data -  [$\color{blue}{\t{Visualizer}}$](http://localhost:8501/Visualizor)
+
+- $\color{#d62728}{\t{BarPlot}}$ for population in thousands
+  - Filtering by gender
+  - populatuin with specfic country - showing the Age bucket wise population
+  - populatuin with all countries
+  - potential to select specfic age buckets - dropping
+- $\color{#d62728}{\t{BoxPlot}}$ for population in thousands
+  - females to Country
+  - males to Country
+  - Both
+""")
+    
+#* ===================================================== bar plot ================================================================================================================
+    st.write("## 1 - BarPlots")
+    st.markdown("""
+let's get our data ready for plotting first
+age_dict is a dictionary created in [Utils.py](Utils.py)
+creating a new df using the dictionary age_dict
+
+```python
+age_values = pd.DataFrame(age_dict.keys(), columns=['age_bucket'])
+age_values['values'] = age_dict.values()
+```
+merging the age_gender df with the age_values df giving age_bucket a value from 0 to 19
+```python
+age_gender = age_gender.merge(age_values, on='age_bucket', how='inner')
+age_gender.sort_values(by='values', ascending=True, inplace=True)
+```
+now let's take a look at the dataframe
+```python
+age_gender
+```
+                """)
+    age_values = pd.DataFrame(age_dict.keys(), columns=['age_bucket'])
+    age_values['values'] = age_dict.values()
+    age_gender = age_gender.merge(age_values, on='age_bucket', how='inner')
+    age_gender.sort_values(by='values', ascending=True, inplace=True)
+    st.dataframe(age_gender)
+
+    st.markdown("""
+We have implemented a function that will a barplot for the data on our prefrence
+ - for info on the function see Utils.py
+ - the plot is done using [matplotlib](https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.bar.html)
+
+```python
+bar_plot(df=age_gender, choosen_gender='male', destination='AU')
+```
+""")
+    bar_plot(df=age_gender, choosen_gender='male', destination='AU')
+
+    st.markdown("""
+```python
+bar_plot(df=age_gender, choosen_gender='female', destination='AU')
+```
+""")
+    bar_plot(df=age_gender, choosen_gender='female', destination='AU')
+    st.markdown("""
+```python
+bar_plot(df=age_gender, choosen_gender='both', destination='AU')
+```
+""")
+    bar_plot(df=age_gender, choosen_gender='both', destination='AU')
+    
+#* =================================================== Box plot =============================================================
+
+
+    st.write("## 2 - BoxPlots")
+    st.markdown("""
+boxplot is a standart way to how data is distributed but are going to try anther approch first
+- df.describe()
+
+```python
+age_gender[(age_gender['country_destination'] == 'AU')&
+            (age_gender['gender'] =='male')].describe()['population_in_thousands'].round(2)
+```
+""")
+    st.dataframe(age_gender[(age_gender['country_destination'] == 'AU') & (
+        age_gender['gender'] == 'male')].describe()['population_in_thousands'])
+    st.markdown("""
+now let's try a box plot, this time we are using [Plotly](https://plotly.com/python/box-plots/) instead of matplotlib
+
+this is a simple example of box plot function
+```python
+box_plot_vertical(df=age_gender, choosen_gender='both', destination='AU', orientation='v')
+check visualizer for more plots
+```
+""")
+    box_plot(df=age_gender, choosen_gender='both',
+                      destination='AU', orientation='v')
+
+    st.markdown("""
+## $\color {red} {\t {Insights}}$
+- there are Countries prefered than others
+- we will be showing why in the next Dataset using Heatmaps 
+""")
+
+#* =================================================== 2- Countries DF =============================================================
+
+    st.markdown("""## $\color{#1f77b4}{\t{Countries DataSet}}$
+                
+#### let's take a look at the data
+```python
+countries = pd.read_csv('airbnb\countries.csv')
+```
+                """)
+    countries = pd.read_csv('airbnb\countries.csv')
+    st.dataframe(countries)
+    st.markdown("""
+## $\color{blue}{\t{Notes}}$
+- ~~lat_destination~~ = $\color{green}{\t{latitude}}$ $\color{red}{\t{redundant}}$ 
+- ~~lng_destination~~ = $\color{green}{\t{longitude}}$ $\color{red}{\t{redundant}}$ 
+- ~~destination_km2~~ is $\color{red}{\t{redundant}}$ 
+- $\color{orange}{\t{levenshtein distance}}$ , In information theory, linguistics, and computer science, the Levenshtein distance is a string metric for measuring the difference between two sequences. Informally, the Levenshtein distance between two words is the minimum number of single-character edits required to change one word into the other.
+- **language_levenshtein_distance** =  $\color{lightgreen}{\t{language difficulty}}$ 
+- drop $\color{Red}{\t{US}}$ row , as it doesn't have much information to the analysis, and will miss up the calculation of correlation
+- giving each country $\color{lightgreen}{\t{Rank by nearst country}}$ of distance from the country to US
+- giving each country $\color{lightgreen}{\t{Rank by Travellers}}$ of people traveling to that country
+""")
+
+    st.markdown("""
+```python
+#* let's drop useless columns , gorupby and sort the dataframe
+countries = countries.drop(['lat_destination','lng_destination','destination_km2'], axis=1)
+population = age_gender.groupby('country_destination')['population_in_thousands'].sum().reset_index()
+countries = countries.merge(population, on='country_destination', how='inner')
+countries.drop([9], axis=0, inplace=True)
+
+#* renaming the columns
+countries.rename(columns={'language_levenshtein_distance': 'language difficulty'}, inplace=True)
+countries.rename(columns={'destination_language ': 'destination language'}, inplace=True)
+countries.rename(columns={'population_in_thousands': 'Travellers'}, inplace=True)
+
+#* remaping Country names
+countries['country_destination'] = countries['country_destination'].map(countries_dict)
+countries.reset_index(drop=True, inplace=True)
+
+#* adding new column for ranking 
+countries.sort_values(by='Travellers', ascending=False, inplace=True)
+countries['Rank by Travellers'] = range(1, len(countries)+1)
+countries.sort_values(by='distance_km', ascending=True, inplace=True)
+countries['Rank by nearst country'] = range(1, len(countries)+1)
+
+```
+
+now let's take a look at the dataframe
+
+```python
+countries
+``` 
+""")
+
+    # * let's drop useless columns , gorupby and sort the dataframe
+    countries = countries.drop(
+        ['lat_destination', 'lng_destination', 'destination_km2'], axis=1)
+    population = age_gender.groupby('country_destination')[
+        'population_in_thousands'].sum().reset_index()
+    countries = countries.merge(
+        population, on='country_destination', how='inner')
+    countries.drop([9], axis=0, inplace=True)
+
+    # * renaming the columns
+    countries.rename(
+        columns={'language_levenshtein_distance': 'language difficulty'}, inplace=True)
+    countries.rename(
+        columns={'destination_language ': 'destination language'}, inplace=True)
+    countries.rename(
+        columns={'population_in_thousands': 'Travellers'}, inplace=True)
+
+    # * remaping Country names
+    countries['country_destination'] = countries['country_destination'].map(
+        countries_dict)
+    countries.reset_index(drop=True, inplace=True)
+
+    # * adding new column for ranking
+    countries.sort_values(by='Travellers', ascending=False, inplace=True)
+    countries['Rank by Travellers'] = range(1, len(countries)+1)
+    countries.sort_values(by='distance_km', ascending=True, inplace=True)
+    countries['Rank by nearst country'] = range(1, len(countries)+1)
+
+    st.dataframe(countries)
+    
+    
+#* =================================================== Approch 1 =============================================================
+
+    st.markdown("""
+let's try some analysis on the data
+## $\color {#2ca02c} {\t {Approch 1 }}$
+ - relation between Traverllers and language difficulty
+ ```python
+Lang_corr = countries.groupby('destination language')[['Travellers', 'language difficulty']].sum(
+).sort_values(by='Travellers', ascending=False)
+Lang_corr.sort_values(by='language difficulty', ascending=True, inplace=True)
+```
+let's take a look at our new Language correlation table
+```python
+Lang_corr
+```
+    """)
+    Lang_corr = countries.groupby('destination language')[['Travellers', 'language difficulty']].sum(
+    ).sort_values(by='Travellers', ascending=False)
+    Lang_corr.sort_values(by='language difficulty', ascending=True, inplace=True)
+
+    st.dataframe(Lang_corr)
+    
+    st.markdown("""
+let's apply .corr() on dataframe to see the relation betweeen columns
+```python
+Lang_corr.corr()
+```
+""")
+    st.dataframe(Lang_corr.corr())
+    st.markdown("""
+now let's plot it on a heatmap
+```python
+heatmap_plottly(Lang_corr)
+```
+heatmap_plottly is a function that plots a heatmap of the correlation matrix of the dataframe
+check utils.py for more details
+""")
+    heatmap_plottly(Lang_corr)
+    
+#* =================================================== Approch 2 =============================================================
+
+    
+    st.markdown("""
+## $\color {#2ca02c} {\t {Approch 2 }}$
+ - relation between Traverllers and Distance from US
+ 
+ ```python
+dist_corr = countries[['Travellers','distance_km','country_destination' ]]
+dist_corr.set_index('country_destination', inplace=True)
+```
+let's take a look at our new Distance correlation table
+```python
+dist_corr
+```
+ """)
+    dist_corr = countries[['Travellers','distance_km','country_destination' ]]
+    dist_corr.set_index('country_destination', inplace=True)
+    st.dataframe(dist_corr)
+    
+    st.markdown("""
+let's apply .corr() on dataframe to see the relation betweeen columns
+```python
+dist_corr.corr()
+```
+""")
+    st.dataframe(dist_corr.corr())
+    st.markdown("""
+now let's plot it on a heatmap
+```python
+heatmap_plottly(dist_corr)
+```
+heatmap_plottly is a function that plots a heatmap of the correlation matrix of the dataframe
+check utils.py for more details
+""")
+    heatmap_plottly(dist_corr)
+    st.markdown("""
+## $\color {red} {\t {Insights}}$
+- it's clear that the language difficulty is dominating the analysis
+- when the language difficulty increases, the number of travellers decreases 
+- distance is a factor in the analysis but it's weight is low compared to language difficulty
+""")
+
+
+
+if __name__ == "__main__":
+    run()
