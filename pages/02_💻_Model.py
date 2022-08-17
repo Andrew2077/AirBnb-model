da5dfb23b534d7192c33ec522842cca24c893419
Andrew Muhsen Paramedis <92538925+Andrew2077@users.noreply.github.com>
2022-08-17 05:13:41 +0200

    model

---
+++ pages/02_💻_Model.py
@@ -0,0 +1,11 @@
+from Utils import*
+
+st.set_page_config(
+     page_title="AirBnb prediction model",
+     page_icon="💻",
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
+st.markdown("""
+$\color{red}{\t{Stay tuned}}$
+            """)
