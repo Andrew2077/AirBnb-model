c758dcd802ee632a58a8dc65fab1add5be6e8d94
Andrew Muhsen Paramedis <92538925+Andrew2077@users.noreply.github.com>
2022-09-10 21:08:26 +0200

    Update 04_Bonus_Q.py

--- pages/04_Bonus_Q.py
+++ pages/04_Bonus_Q.py
@@ -2,8 +2,9 @@ from Utils import*
 
 st.markdown("""## $\color{#69109c}{\t{Outlayers - Robusticity}}$ 
             
-- ##### $\color{#439c10}{\t{1- Neural networks, RNN or CNN}}$ 
-- ##### $\color{#439c10}{\t{2- Naive bayes}}$ 
-- ##### $\color{#439c10}{\t{3- probabilty built models}}$ 
-- ##### $\color{#439c10}{\t{3- Trees, random forests, XGBoost, AdaBoost}}$ 
+- ##### $\color{#439c10}{\t{ 1- Neural networks, RNN or CNN}}$ 
+- ##### $\color{#439c10}{\t{ 2- Naive bayes}}$ 
+- ##### $\color{#439c10}{\t{ 3- probabilty built models}}$ 
+- ##### $\color{#439c10}{\t{ 4- Trees, random forests, XGBoost, AdaBoost}}$ 
+
 """)
